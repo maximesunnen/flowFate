f78c76a0fca12ac14feca43fe2960eef0f1d54ce
maximesunnen <maximesunnen@gmail.com>
2023-03-11 17:56:45 +0100

    added comments for visual organisation replaced ifelse() with dplyr::case_when() because ifelse only returns a vector of the same length as the logical argument

--- R/mod_curate.R
+++ R/mod_curate.R
@@ -13,27 +13,37 @@
 
 mod_curate_ui <- function(id){
   ns <- NS(id)
-    tabPanel(title = "Curate",
-             sidebarLayout(
-      sidebarPanel(
-        uiOutput(ns("KRAS_selection")),
-        uiOutput(ns("MYHC_channel")),
-        uiOutput(ns("negative_control")),
-        uiOutput(ns("KRAS_control")),
-        uiOutput(ns("MYHC_control"))
-        ),
-      
-    mainPanel(
-      
-      h1("How curation works."),
-      
-      p("By curation we understand two essential steps. First, we want to focus our analysis on intact cells and not debris. We therefore need to set a gate that excludes cellular debris, which normally clusters in the lower left corner in a SSC vs FSC plot. Second, we have to define intensity thresholds in our fluorescent channels below which we cannot distinguish between a real signal and autofluorescence/background noise. We will define both the non-debris gate and the threshold using our controls."),
-      
-      actionButton("Curate", "Start curation"),
-      plotOutput("ssc_fsc"),
-      textOutput(ns("cur_ds")),
-      textOutput(ns("test"))
-    )))}
+  
+  # Defining a tabPanel layout ----------------------------------------------
+  tabPanel(title = "Curate",
+           
+           # Defining a sidebarLayout in the Curate tab ------------------------------
+           sidebarLayout(
+             sidebarPanel(
+
+               # Input selections for used channels and control samples ------------------
+               uiOutput(ns("KRAS_selection")),
+               uiOutput(ns("MYHC_channel")),
+               uiOutput(ns("negative_control")),
+               uiOutput(ns("KRAS_control")),
+               uiOutput(ns("MYHC_control"))
+             ),
+             
+             mainPanel(
+
+               # header and text description of curation ---------------------------------
+               h1("How curation works."),
+               curate_text,
+               
+               # Action button to start curation
+               actionButton("Curate", "Start curation"),
+               
+               # plot SSC vs FSC for control samples -------------------------------------
+               plotOutput("controls_ssc_fsc"),
+               
+               textOutput(ns("cur_ds")),
+               textOutput(ns("test"))
+             )))}
 
 #' curate Server Functions
 #'
@@ -42,7 +52,8 @@ mod_curate_ui <- function(id){
 mod_curate_server <- function(id,r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-
+    
+    # All selections from sidebar (control datasets and channels) -------------
     output$KRAS_selection <- renderUI({
       selectInput("kras_channel", 
                   "KRas channel", 
@@ -58,27 +69,44 @@ mod_curate_server <- function(id,r){
     output$negative_control <- renderUI({
       selectInput("negative_dataset", 
                   "Negative control", 
-                  choices = ifelse(is_null(r$fs), "" , rownames(pData(r$fs))),
-                  selected = c())
+                  choices = dplyr::case_when(is_null(r$fs)~"",
+                                             !is_null(r$fs)~ rownames(pData(r$fs))))
     })
     
     output$KRAS_control <- renderUI({
       selectInput("kras_dataset", 
                   "Positive control (KRAS)", 
-                  choices = ifelse(is_null(r$fs), "" , rownames(pData(r$fs))))
+                  choices = dplyr::case_when(is_null(r$fs)~"",
+                                             !is_null(r$fs)~ rownames(pData(r$fs))))
     })
 
     output$MYHC_control <- renderUI({
       selectInput("myhc_dataset", 
                   "Positive control (MYHC)", 
-                  choices = ifelse(is_null(r$fs), "" , rownames(pData(r$fs))),
-                  selected = "")
+                  choices = dplyr::case_when(is_null(r$fs)~"",
+                                             !is_null(r$fs)~ rownames(pData(r$fs))))
     })
 
+    # SSC vs FSC plot of control samples --------------------------------------
+    ## get indices of the datasets selected
+    control_indices <- reactive(c(input$negative_dataset, 
+                                  input$kras_dataset, 
+                                  input$myhc_dataset))
     
+    output$controls_ssc_fsc <- renderPlot({
+      ggcyto(r$gs[[control_indices]], aes(x = SSC.HLin, y = FSC.HLin), subset = "root") +
+        geom_hex(bins = 150) +
+        theme_bw()
+    })
+      
     output$test <- renderText({glue::glue("You selected dataset number {r$s()}")})
         })
-  }
+}
+
+
+
+
+curate_text <- glue("By curation we understand two essential steps. First, we want to focus our analysis on intact cells and not debris. We therefore need to set a gate that excludes cellular debris, which normally clusters in the lower left corner in a SSC vs FSC plot. Second, we have to define intensity thresholds in our fluorescent channels below which we cannot distinguish between a real signal and autofluorescence/background noise. We will define both the non-debris gate and the threshold using our controls.")
 
 ## To be copied in the UI
 # mod_curate_ui("curate_1")
