2f81e78891907919bc051b60b4b75fc38b7b16da
maximesunnen <maximesunnen@gmail.com>
2023-03-15 16:19:53 +0100

    tried wrapping it up in an observer + bindEvent command. Apparently eventReactive() should be replaced with this strategy as of Shiny 1.6.0

--- R/mod_curate.R
+++ R/mod_curate.R
@@ -55,6 +55,8 @@ mod_curate_ui <- function(id){
 #' @import ggplot2
 #' @importFrom ggcyto ggcyto geom_gate geom_stats
 #' @import flowWorkspace
+
+
 mod_curate_server <- function(id,r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
@@ -109,69 +111,82 @@ mod_curate_server <- function(id,r){
     # SSC vs FSC plot of control samples --------------------------------------
     ## get indices (is it really indices???) of the datasets selected
     
-    #compute all these reactive expressions only when input$Curate is activated
-    ## this also means that code depending on e.g. control_indices() will not update unless you click on curate [results are cached, and before input$Curate, this code would not know that the input has changed]
-
+    #compute all these reactive expressions only when input$Curate is activated: so only dependency on input$Curate, not on the changing of the selectInput. This also means that code depending on e.g. control_indices() will not update unless you click on curate [results are cached, and before input$Curate, this code would not know that the input has changed]
+    ## question: is my understanding here correct? I think that with eventReactive you create a reactive dependency on the first argument (here input$Curate), so when this updates, the entire code is computed, regardless of the fact that input$kras_control etc changed or not?
+    
     control_indices <- eventReactive(input$Curate, c(input$kras_control, 
                                                      input$myhc_control, 
                                                      input$negative_control))
+    
     ssc <- eventReactive(input$Curate, {input$ssc_channel})
     fsc <- eventReactive(input$Curate, {input$fsc_channel})
     
-    # define a reactive expression which generates the gate, since it depends on ssc() and fsc() (which themselves depend on input$Curate), they don't trigger unless they have input.
-    # However, I have no idea why this is not giving a sort of "don't know how to deal with objecte
-    # of type NULL error....)
-    # -->  now i know why: ssc() has an initial value, since i added "selected =... " argument to selectInput, so initially ssc() does exist and is not NULL!!!
-    # ideally, pgn-cut should  be out of the reactive, since it does not depend on any reactive input(and it does not have to be recomputed every time ssc() changes!
-    #  polygonGate however HAS to be inside, because it uses ssc() and fsc()
+
+# define the polygon gate matrix ------------------------------------------
+  # can maybe be placed outside the server function
     
     pgn_cut <- matrix(c(0, 12500, 99000, 99000,0,6250, 6250, 6250, 99000, 99000),
                       ncol = 2,
                       nrow = 5)
     
-    #very important question: when ssc() changes (because selectInput changed and input$Curate was activated, is polygonGate updated (is think yes)? or does pgn_cut have to be a reactive for this and used in polygonGate as .gate = pgn_cut()) (i think no)
+    # define a reactive expression which generates the gate, since it depends on ssc() and fsc() (which themselves depend on input$Curate), they don't trigger unless they have input.
+    # However, I have no idea why this is not giving a sort of "don't know how to deal with object of type NULL" error....)
+    # -->  now i know why: ssc() has an initial value, since i added "selected =... " argument to selectInput, so initially ssc() does exist and is not NULL!!! BUT only after "Curate"???)
+    # ideally, pgn-cut should  be out of the reactive, since it does not depend on any reactive input(and it does not have to be recomputed every time ssc() changes!
+    #  polygonGate however HAS to be inside, because it uses ssc() and fsc()
+    
+    #very important question: when ssc() changes (because selectInput changed and input$Curate was activated, is polygonGate updated (i think yes)? or does pgn_cut have to be a reactive for this and used in polygonGate as .gate = pgn_cut()) (i think no)
     #exlude debris should not be in observe({}) since you can't use observers in other statements, they're made for their side effects!
-    #
 
     exclude_debris <- reactive({
       colnames(pgn_cut) <- c(ssc(), fsc())
       polygonGate(filterId = "NonDebris", .gate = pgn_cut)
     })
 
-
-                         
     
   #control_incides() is under the control of input$Curate. It's funny that i don't get an error of the type: "Can't subset r$gs" because initially control_indices() does not exist. does this have to do with lazyness? render*_ functions only compute their content when it's necessary? when drawing the reactive graph it makes sense! ssc() cannot be computed, therefore exclude_debris() cannot be computed, and ultimately the renderPlot({}) is stuck at the first line and will not try to compute ggcyto (which would lead to an error?)
-## is this really good practice here to put other stuff than the actual plot inside renderPlot()?
 
+    ## is this good practice here to put other stuff than the actual plot inside renderPlot()?
+    
+  # output$non_debris_gate <- renderPlot({
+  #   # gate_non_debris <- 
+  #   #exclude_debris()
+  #   message("okay")
+  #   # add gate to gs
+  #   gs_pop_add(r$gs, exclude_debris(), parent = "root")
+  #   # recompute the GatingSet
+  #   message("Added the non_debris gate to the gatingSet")
+  #   recompute(r$gs)
+  #   message("Recomputed the gatingSet")
+  #   ggcyto(r$gs[[control_indices()]],
+  #          aes(x = .data[[ssc()]] , y = .data[[fsc()]]),
+  #          subset = "root") +
+  #     geom_hex(bins = 150) +
+  #     theme_bw() 
+  #     geom_gate(exclude_debris())
+  #     geom_stats()
+  # })
+  # 
+
+observe({
+  colnames(pgn_cut) <- c(ssc(), fsc())
+  gate_non_debris <- polygonGate(filterId = "NonDebris", .gate = pgn_cut)
+  message("created the gate")
+  # add gate to gs
+  gs_pop_add(r$gs, exclude_debris(), parent = "root")
+  # recompute the GatingSet
+  message("Added the non_debris gate to the gatingSet")
+  recompute(r$gs)
+  message("Recomputed the gatingSet")
   output$non_debris_gate <- renderPlot({
-    # gate_non_debris <- 
-    exclude_debris()
-    # add gate to gs
-    gs_pop_add(r$gs, exclude_debris(), parent = "root")
-    # recompute the GatingSet
-    message("Added the non_debris gate to the gatingSet")
-    recompute(r$gs)
     ggcyto(r$gs[[control_indices()]],
-           aes(x = .data[[ssc()]] , y = .data[[fsc()]]),
-           subset = "root") +
-      geom_hex(bins = 150) +
-      theme_bw() +
-      geom_gate(exclude_debris()) +
-      geom_stats()
-  })
-  
-
-#   output$gfp_gate <- renderPlot({
-#   gate_gfp <- 
-#     ggcyto(r$gs[[control_indices()[c(1,2)]]],
-#            aes(x = .data[[ssc()[1]]]),
-#            subset = "root") +
-#       geom_density(fill = "forestgreen") +
-#       theme_bw() +
-#       geom_gate(gate_gfp) +
-#       geom_stats()
-# }) |> bindEvent(input$Curate, ignoreInit = TRUE)
+         aes(x = .data[[ssc()]] , y = .data[[fsc()]]),
+         subset = "root") +
+    geom_hex(bins = 150) +
+    theme_bw()+
+    geom_gate(gate_non_debris)+
+    geom_stats()
+})}) |> bindEvent(input$Curate, ignoreInit = TRUE)
 
 
   })
