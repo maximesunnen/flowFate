404a01c752a5cbae8a84a3a3283291a74719eed1
maximesunnen <maximesunnen@gmail.com>
2023-03-25 10:31:19 +0100

    new module

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -21,7 +21,8 @@ attachment::att_amend_desc()
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module(name = "import", with_test = TRUE) # Name of the module
-golem::add_module(name = "curate", with_test = TRUE) # Name of the module
+golem::add_module(name = "curate", with_test = TRUE)# Name of the module
+golem::add_module(name = "gate", with_test = TRUE)# Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
