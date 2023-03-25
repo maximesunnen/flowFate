bc5e86270c731ecaf7b9cb935dd6b49b994ce2ee
maximesunnen <maximesunnen@gmail.com>
2023-03-25 11:50:04 +0100

    added option to add additional bins (incomplete)

--- R/mod_gate.R
+++ R/mod_gate.R
@@ -7,12 +7,28 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
+#' @importFrom shinyWidgets numericRangeInput actionBttn
 mod_gate_ui <- function(id){
   ns <- NS(id)
-  tagList(
- 
-  )
-}
+  tabPanel(title = "Gate",
+           
+           # Defining a sidebarLayout in the Curate tab ------------------------------
+           sidebarLayout(
+             sidebarPanel(
+               numericRangeInput(ns("gfp_bin_1"), label = "First GFP bin", value = c(100, 350)),
+               uiOutput(ns("gfp_bin_2")),
+               actionButton(inputId = ns("add_input"), icon("plus"))
+
+             ),
+             
+             mainPanel(
+               
+               # header and text description of curation ---------------------------------
+               h1("How gating works."),
+               p(),
+
+             )))}
+
     
 #' gate Server Functions
 #'
@@ -20,6 +36,8 @@ mod_gate_ui <- function(id){
 mod_gate_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+    observeEvent(input$add_input,
+                 output$gfp_bin_2 <- renderUI(numericRangeInput(ns("test"), label = "Second GFP bin", value = c(100, 350))))
  
   })
 }
