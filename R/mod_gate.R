17cf903bcc0c5baa44c864a11a45114053651c61
maximesunnen <maximesunnen@gmail.com>
2023-04-06 10:06:55 +0200

    apparently useShinyjs() has to be added into every module it will be used.
    addedsome text outputs to check if everything is working as expected.
    
    added the option that the user can click on a button if he wants additional gfp bins

--- R/mod_gate.R
+++ R/mod_gate.R
@@ -10,14 +10,17 @@
 #' @importFrom shinyWidgets numericRangeInput actionBttn
 mod_gate_ui <- function(id){
   ns <- NS(id)
+  useShinyjs()
+  
   tabPanel(title = "Gate",
            
            # Defining a sidebarLayout in the Curate tab ------------------------------
            sidebarLayout(
              sidebarPanel(
-               numericRangeInput(ns("gfp_bin_1"), label = "First GFP bin", value = c(100, 350)),
+               uiOutput(ns("gfp_bin_1")),
                uiOutput(ns("gfp_bin_2")),
-               actionButton(inputId = ns("add_input"), icon("plus"))
+               uiOutput(ns("gfp_bin_3")),
+               actionButton(inputId = ns("add_input"), label = "Add GFP bins", icon("plus"))
 
              ),
              
@@ -26,6 +29,7 @@ mod_gate_ui <- function(id){
                # header and text description of curation ---------------------------------
                h1("How gating works."),
                p(),
+               textOutput(ns("bin_ranges"))
 
              )))}
 
@@ -33,12 +37,45 @@ mod_gate_ui <- function(id){
 #' gate Server Functions
 #'
 #' @noRd 
-mod_gate_server <- function(id){
+#' @importFrom shinyjs show hide useShinyjs
+mod_gate_server <- function(id, r){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    observeEvent(input$add_input,
-                 output$gfp_bin_2 <- renderUI(numericRangeInput(ns("test"), label = "Second GFP bin", value = c(100, 350))))
+    
+    observe({
+      if (is.null(r$lower_limit_gfp)) {shinyjs::hide(id = "add_input")}
+      else {shinyjs::show(id = "add_input")}
+      })
+    
+    
+    observe({
+      if (input$add_input == 1) {
+      output$gfp_bin_1 <- renderUI(numericRangeInput(ns("gfp_range_1"), label = "First GFP bin", value = c(signif(r$lower_limit_gfp, digits = 3),100)))
+      }
+      if (input$add_input == 2) {
+        output$gfp_bin_2 <- renderUI(numericRangeInput(ns("gfp_range_2"), label = "Second GFP bin", value = c(101,350)))
+      }
+      if (input$add_input == 3) {
+        output$gfp_bin_3 <- renderUI(numericRangeInput(ns("gfp_range_3"), label = "Third GFP bin", value = c(351,1000)))
+        shinyjs::hide(id = "add_input")
+      }
+    }) |> bindEvent(input$add_input)
+    
+    
+    output$bin_ranges <- renderText({
+      if (is.null(input$gfp_range_1)) return(NULL)
+        else if (!is.null(input$gfp_range_1) & is.null(input$gfp_range_2)) {
+          paste0("Your first bin ranges from ", input$gfp_range_1[1], " to ", input$gfp_range_1[2], ".")
+        }
+      else if (!is.null(input$gfp_range_2) & is.null(input$gfp_range_3)) {
+        paste0("Your first bin ranges from ", input$gfp_range_1[1], " to ", input$gfp_range_1[2], ".", " Your second bin ranges from", input$gfp_range_2[1], " to ", input$gfp_range_2[2])}
+      else if (!is.null(input$gfp_range_3)) {
+        paste0("Your first bin ranges from ", input$gfp_range_1[1], " to ", input$gfp_range_1[2], ".", " Your second bin ranges from", input$gfp_range_2[1], " to ", input$gfp_range_2[2],". ", "Your third bin ranges from ", input$gfp_range_3[1], " to ", input$gfp_range_3[2]," ." )
+      }
+      })
  
+    
+    
   })
 }
     
