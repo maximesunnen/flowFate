68c1958d922c374a936f2dba220918f34f8d2c78
maximesunnen <maximesunnen@gmail.com>
2023-04-08 00:51:26 +0200

    not crashing but not working correctly - maaaaany warnings - plot looks weird in gate module

--- R/mod_gate.R
+++ R/mod_gate.R
@@ -20,7 +20,7 @@ mod_gate_ui <- function(id){
                 uiOutput(ns("gfp_bin_2")),
                 uiOutput(ns("gfp_bin_3")),
                 actionButton(inputId = ns("add_input"), label = "Add GFP bins", icon("plus")),
-                actionButton(inputId = ns("Gate"), label = "Gate now")
+                actionButton(inputId = ns("gate"), label = "Gate now")
              ),
              
              mainPanel(
@@ -54,82 +54,81 @@ mod_gate_server <- function(id, r){
       if (is.null(input$add_input)) return(NULL)
       if (input$add_input == 1) {
         render_bin_UI(1, c(signif(r$lower_limit_gfp, digits = 3), 100), ns, "First", output, r)
-        # output$gfp_bin_1 <- renderUI(numericRangeInput(ns("gfp_range_1"), label = "First GFP bin (LOW)", value = c(signif(r$lower_limit_gfp, digits = 3),100)))
       }
       if (input$add_input == 2) {
         render_bin_UI(2, c(101,350), ns, "Second", output, r)
-      #   output$gfp_bin_2 <- renderUI(numericRangeInput(ns("gfp_range_2"), label = "Second GFP bin (MEDIUM)", value = c(101,350)))
       }
       if (input$add_input == 3) {
-        render_bin_UI(3, c(351,1000), ns, "Second", output, r)
-      #   output$gfp_bin_3 <- renderUI(numericRangeInput(ns("gfp_range_3"), label = "Third GFP bin (HIGH)", value = c(351,1000)))
+        render_bin_UI(3, c(351,1000), ns, "Third", output, r)
         shinyjs::hide(id = "add_input")
       }
     }) |> bindEvent(input$add_input)
-    
 
-    
-    
     ## SET UP A GATE ACCORDING TO THE USER-DEFINED BIN RANGE
     observe({
       if (is.null(input$add_input)) return(NULL)
-      
-      if (!is.null(input$gfp_range_1) && !is.null(input$gfp_range_2) && !is.null(input$gfp_range_3)) {
-        # set gate limits (using user-defined ranges)
-        gate_limits <- list(low = list(c(input$gfp_range_1[1], input$gfp_range_1[2])),
-                            medium = list(c(input$gfp_range_2[1], input$gfp_range_2[2])),
-                            high = list(c(input$gfp_range_3[1], input$gfp_range_3[2])))
-        ### for testing
-        print(gate_limits)
-        print(r$kras_channel())
-        
-        # generate gates from the bin sizes
+      if (!is.null(input$gfp_range_3)) {
+        # set up gate limits (using user-defined ranges)
+        gate_limits <- list(low = list(input$gfp_range_1), medium = list(input$gfp_range_2), 
+                            high = list(input$gfp_range_3))
+        # generate gates from bins
         gates <- lapply(gate_limits, function(x) {
           names(x) <- r$kras_channel()
           rectangleGate(x)
         })
-        
-        # vector of filter names
-        filter_names <- c("GFP-low", "GFP-medium", "GFP-high")
-        print(gates)
-        
-        # assign new filterIds to the generated gates (maybe wrap all this into a smart function to reduce codebase)
+        # assign filterIds to gates
         for (i in seq_along(gates)) {
+          filter_names <- c("GFP-low", "GFP-medium", "GFP-high")
           gates[[i]]@filterId <- filter_names[i]
         }
-        
-        # add gates to the gatingSet
-        for (i in seq_along(gates)) {
-          gs_pop_add(r$gs, gates[[i]], parent = "MYO+")
-        }
-        
-        recompute(r$gs)
-        ### for testing
-        plot(r$gs)
-        
-        data_gfp_low <- gs_pop_get_data(r$gs, y = filter_names[1]) |> cytoset_to_flowSet()
-        data_gfp_medium <- gs_pop_get_data(r$gs, y = filter_names[2]) |> cytoset_to_flowSet()
-        data_gfp_high <- gs_pop_get_data(r$gs, y = filter_names[3]) |> cytoset_to_flowSet()
-        print(data_gfp_low)
+      }
+    }) |> bindEvent(input$gate)
 
-      gfp_low_myo_high <- fsApply(data_gfp_low, test_function)
-      gfp_low_myo_high <- gfp_low_myo_high[-which(sapply(gfp_low_myo_high, is.null))]
 
-      output$test_plot <- renderPlot({
-        ggcyto(r$gs[[5]], aes(x = "RED.R.HLin"), subset = "GFP-low") +
-          geom_density(fill = "forestgreen") +
-          scale_x_flowjo_biexp() +
-          theme_bw() +
-          geom_gate(gfp_low_myo_high)
-      })
+    ## ADD GATES TO GATINGSET
+    observe({
+      for (i in seq_along(gates)) {
+        gs_pop_add(r$gs, gates[[i]], parent = "MYO+")
+      }
+      recompute(r$gs)
+      ### for testing
+      plot(r$gs)
+    }) |> bindEvent(input$gate)
+    
+    observe({
+    data_gfp_low <- gs_pop_get_data(r$gs, y = "GFP-low") |> cytoset_to_flowSet()
+    data_gfp_medium <- gs_pop_get_data(r$gs, y = "GFP-medium") |> cytoset_to_flowSet()
+    data_gfp_high <- gs_pop_get_data(r$gs, y = "GFP-high") |> cytoset_to_flowSet()
+    print(data_gfp_low)
+    
+    gfp_low_myo_high <- fsApply(data_gfp_low, test_function)
+    gfp_low_myo_high <- gfp_low_myo_high[-which(sapply(gfp_low_myo_high, is.null))]
+    
+    gfp_medium_myo_high <- fsApply(data_gfp_medium, test_function)
+    gfp_medium_myo_high <- gfp_medium_myo_high[-which(sapply(gfp_medium_myo_high, is.null))]
+    
+    gfp_high_myo_high <- fsApply(data_gfp_high, test_function)
+    gfp_high_myo_high <- gfp_high_myo_high[-which(sapply(gfp_high_myo_high, is.null))]
+    
+    output$test_plot <- renderPlot({
+      ggcyto(r$gs[[5]], aes(x = "RED.R.HLin"), subset = "GFP-low") +
+        geom_density(fill = "pink") +
+        scale_x_flowjo_biexp() +
+        theme_bw() +
+        geom_gate(gfp_low_myo_high)
+    })
+    }) |> bindEvent(input$gate)
+    
 
+      
     ## EXTRACT GATED DATA (because we want to apply a mindensity function only on the data in the gate (in this case bin))
     
     output[["test"]] <- renderText(glue("Test works"))
-      }
-      })
-  })}
+
     
+  })}
+
+
 render_bin_UI <- function(bin_number, value, ns, label, output , r) {
   output[[paste0("gfp_bin_", bin_number)]] <- renderUI(numericRangeInput(ns(paste0("gfp_range_", bin_number)), label = paste0(label, "GFP bin"), value = value))
 }
