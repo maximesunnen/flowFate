4488e44ba612451c532d38c54f7d4d9ca657393f
Aurelien Ginolhac <ginolhac@gmail.com>
2023-08-10 15:34:45 +0200

    add tests for curate and gating

--- tests/testthat/test-mod_curate.R
+++ tests/testthat/test-mod_curate.R
@@ -27,10 +27,14 @@ testServer(
     pgn_cut <- matrix(c(12500, 99000, 99000,0,0,6250, 6250, 99000, 99000,12500), ncol = 2, nrow = 5)
     colnames(pgn_cut) <- c("SSC.HLin", "FSC.HLin")
     gate_non_debris <- polygonGate(filterId = "NonDebris", .gate = pgn_cut)
-    # lower_limit_gfp_gate <- get_lowerLimit(gs = gs,
-    #                                        datasets = c("00_Double-negative.fcs",
-    #                                                     "02_G12C_Untreated.fcs"),
-    #                node = "NonDebris", ch_gate = "RED.R.HLin", r = r)
+    nb_nodes <- gs_pop_add(gs, gate_non_debris, parent = "root")
+    expect_true(nb_nodes == 2L)
+    expect_message(recompute(gs), "done!")
+    lower_limit_gfp_gate <- get_lowerLimit(gs = gs,
+                                           datasets = c("00_Double-negative.fcs",
+                                                        "01_MyHC_plus_only.fcs"),
+                                           node = "NonDebris", ch_gate = "GRN.B.HLin", r = r)
+    expect_equal(lower_limit_gfp_gate, 24.1406016635)
 
 })
 
--- tests/testthat/test-mod_gate.R
+++ tests/testthat/test-mod_gate.R
@@ -13,17 +13,39 @@ testServer(
     expect_true(
       grepl("test", ns("test"))
     )
-    # Here are some examples of tests you can
-    # run on your module
-    # - Testing the setting of inputs
-    # session$setInputs(x = 1)
-    # expect_true(input$x == 1)
-    # - If ever your input updates a reactiveValues
-    # - Note that this reactiveValues must be passed
-    # - to the testServer function via args = list()
-    # expect_true(r$x == 1)
-    # - Testing output
-    # expect_true(inherits(output$tbl$html, "html"))
+    # loading from the demo folder
+    demo_files <- system.file("demo_data_FCS3.0",
+                              package = "flowFate")
+    demo_fs <- read.flowSet(path = demo_files,
+                            truncate_max_range = FALSE,
+                            alter.names = TRUE,
+                            transformation = FALSE,
+                            emptyValue = FALSE)
+    gs <- GatingSet(demo_fs)
+    r <- list(gs = gs)
+    pgn_cut <- matrix(c(12500, 99000, 99000,0,0,6250, 6250, 99000, 99000,12500), ncol = 2, nrow = 5)
+    colnames(pgn_cut) <- c("SSC.HLin", "FSC.HLin")
+    gate_non_debris <- polygonGate(filterId = "NonDebris", .gate = pgn_cut)
+    gs_pop_add(gs, gate_non_debris, parent = "root")
+    recompute(gs)
+    # now Gating, first interval 25 - 100
+    test_gate <- list(low = list(c(25, 100)))
+    names(test_gate[[1]]) <- "GRN.B.HLin"
+    y <- rectangleGate(test_gate[[1]])
+    y@filterId <- "GFP-low"
+    gfp_gate <- make_gate(25, "GRN.B.HLin", filterId = "GFP+")
+    nb_nodes <- gs_pop_add(gs, gfp_gate, parent = "NonDebris")
+    expect_true(nb_nodes == 3L)
+    expect_message(recompute(gs), "done!")
+    expect_true(gs_pop_add(gs, y, parent = "GFP+") == 4L)
+    expect_message(recompute(gs), "done!")
+    expect_identical(gs_pop_get_count_fast(gs)$Count,
+                      c(9587L, 153L, 149L, 4044L, 7L , 6L, 6080L, 2977L, 2100L, 5579L, 2944L,
+                        2052L, 6703L, 1493L, 1083L))
+    expect_identical(gs_pop_get_count_fast(gs)$ParentCount,
+                     c(20000L, 9587L, 153L, 20000L, 4044L, 7L, 20000L, 6080L, 2977L,
+                       20000L, 5579L, 2944L, 20000L, 6703L, 1493L))
+    #  ; 101 350 ; 351 - 1000
 })
 
 test_that("module ui works", {
